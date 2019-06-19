627c4d5020ecfff3a7aa06b1e1dbcb29ac99d0ba
Johan Park <parkjohan@gmail.com>
2019-06-19 14:21:38 -0700

    Initial Commit

---
+++ CoreMachineLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMachineLearning
+//
+//  Created by Johan Park on 6/19/19.
+//  Copyright © 2019 Johan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
